a82c887174ca257ce2ac6f6b49550ce5f7872277
zion187 <zion187@naver.com>
2024-09-11 15:43:58 +0900

    woowang

--- hello_world.py
+++ hello_world.py
@@ -3,8 +3,13 @@ def print_hello():
 
 def print_kingfrog():
     print("King Frog")
-    print("King King King Frog")
+    print("King Suuuuper Frog")
+
+def add(x, y):
+    a = x+y
+    return a
 
 if __name__ == '__main__':
     print_hello()
     print_kingfrog()
+    print(add(1,2))
