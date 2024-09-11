caeac98eb5e9126111ea369125a2974787827132
zion187 <zion187@naver.com>
2024-09-11 15:37:43 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,10 @@
 def print_hello():
     print("Hello World!!")
 
+def print_kingfrog():
+    print("King Frog")
+    print("King King King Frog")
+
 if __name__ == '__main__':
     print_hello()
+    print_kingfrog()
